dce227246bcff94aab125ebc43188f53833b3a2b
Paulius101 <p.dambrausko@gmail.com>
2025-04-26 18:56:40 +0300

    added repositories

---
+++ src/main/java/com/example/petservice/repository/ServiceRepository.java
@@ -0,0 +1,9 @@
+package com.example.petservice.repository;
+
+import com.example.petservice.entity.Service;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ServiceRepository extends JpaRepository<Service, Long> {
+}
