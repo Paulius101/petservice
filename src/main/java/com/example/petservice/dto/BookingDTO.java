5fb77bcb2e24ec2625ad0490a27a0b8bf639b623
Paulius101 <p.dambrausko@gmail.com>
2025-04-26 18:47:57 +0300

    added dtos

---
+++ src/main/java/com/example/petservice/dto/BookingDTO.java
@@ -0,0 +1,16 @@
+package com.example.petservice.dto;
+
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@Data
+public class BookingDTO {
+    private Long bookingId;
+    private String petName;
+    private LocalDate startDate;
+    private LocalDate endDate;
+    private String notes;
+    private Long serviceId;
+    private Long userId;
+}
